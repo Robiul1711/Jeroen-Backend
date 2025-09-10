96342765a133a5111f9f4cad5ef077f2cccbd914
Robiul1711 <ashiq.web.dev@gmail.com>
2025-09-10 09:37:35 +0600

    cors error

--- server.js
+++ server.js
@@ -4,11 +4,31 @@ const cors = require("cors");
 const connectDB = require("./config/db.js");
 
 dotenv.config();
-connectDB(); // 🔥 call database connection
+connectDB();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+
+const allowedOrigins = [
+  "https://jereon-qr-code-generator.netlify.app",
+  "http://localhost:3000",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
+// Preflight
+app.options("*", cors());
 
 // ✅ Serve uploaded images
 app.use("/uploads", express.static("uploads"));
@@ -19,4 +39,5 @@ app.use("/api/qr", require("./router/qrRoutes.js"));
 app.use("/api/user", require("./router/userRoutes.js"));
 
 app.listen(5000, () => console.log("🚀 Server running on port 5000"));
+
 
